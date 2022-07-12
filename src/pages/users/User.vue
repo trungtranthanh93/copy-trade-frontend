b584488274ecb5d909a26e7f324b011bfece129c
trungtranthanh93 <trungtranthanh93@users.noreply.github.com>
2022-07-12 19:28:40 +0700

    Update User.vue

--- src/pages/users/User.vue
+++ src/pages/users/User.vue
@@ -58,6 +58,16 @@
               <q-separator />
               <MenuItem title='Thống Kê' caption='' icon='sticky_note_2' iconColor='orange' link='/user/session-statistic-bot' />
             </q-expansion-item>
+            <q-expansion-item :content-inset-level="0.5">
+              <template v-slot:header>
+                <q-item-section avatar>
+                  <q-icon name="import_export" color="orange" />
+                </q-item-section>
+                <q-item-section>BOT Lệnh</q-item-section>
+              </template>
+              <q-separator />
+              <MenuItem title='Thống Kê' caption='' icon='sticky_note_2' iconColor='orange' link='/user/summary-win' />
+            </q-expansion-item>
           </q-expansion-item>
           <q-separator />
           <!-- <q-expansion-item :content-inset-level="0.5">
